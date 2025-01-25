29a2ffab5f1a6f829fadb20e6d46d862e6264a54
MarkVebeer <markveber100@gmail.com>
2025-01-25 16:26:41 +0100

    fix hero on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center px-8 py-24 relative z-10">
       <div className="max-w-[90vw] w-full text-center">
-        <h1 className="text-7xl lg:text-[7rem] font-bold mb-6 px-4 pl-8 whitespace-nowrap">
+        <h1 className="text-5xl sm:text-7xl lg:text-[7rem] font-bold mb-6 px-4 pl-8 whitespace-nowrap">
           <DecryptedText
             text="Véber-Jurassa Márk"
             speed={60}
@@ -13,13 +13,13 @@ export const Hero = () => {
             revealDirection="center"
             animateOn="view"
             characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&*"
-            className="gradient-text tracking-wide"
+            className="gradient-text lg:tracking-wide"
             encryptedClassName="text-white"
           />
         </h1>
       </div>
       
-      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 z-20">
+      <div className="absolute bottom-16 left-1/2 -translate-x-1/2 z-20">
         <div className="w-4 h-4 opacity-30">
           <div className="w-full h-full border-r-2 border-b-2 border-white/60 rotate-45 transform origin-center"></div>
         </div>
